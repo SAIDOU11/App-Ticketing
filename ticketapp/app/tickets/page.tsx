1efc87f3440e52d17ba059388ece4f4b85dba9f3
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-17 16:25:29 +0200

    database

--- ticketapp/app/tickets/page.tsx
+++ ticketapp/app/tickets/page.tsx
@@ -1,4 +1,9 @@
-const Tickets = () => {
+import prisma from "@/prisma/db";
+
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+  console.log(tickets);
+
   return <div className="p-4">Tickets</div>;
 };
 
