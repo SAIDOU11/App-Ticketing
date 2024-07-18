ea02d307878f5dae9f24ae3184f3e64fb02062f2
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-18 13:01:17 +0200

    status component

--- ticketapp/app/tickets/DataTable.tsx
+++ ticketapp/app/tickets/DataTable.tsx
@@ -1,3 +1,4 @@
+import TicketStatusBadge from "@/components/TicketStatusBadge";
 import {
   Table,
   TableBody,
@@ -23,7 +24,9 @@ const DataTable = ({ tickets }: Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>Title</TableHead>
-              <TableHead>Status</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
               <TableHead>Priority</TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
@@ -33,7 +36,11 @@ const DataTable = ({ tickets }: Props) => {
               ? tickets.map((ticket) => (
                   <TableRow key={ticket.id} data-href="/">
                     <TableCell>{ticket.title}</TableCell>
-                    <TableCell>{ticket.status}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
                     <TableCell>{ticket.priority}</TableCell>
                     <TableCell>
                       {ticket.createdAt.toLocaleDateString("fr-FR", {
