59b475c8420abd3d63e6d0989da41eface634513
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-18 15:15:38 +0200

    priority component

--- ticketapp/app/tickets/DataTable.tsx
+++ ticketapp/app/tickets/DataTable.tsx
@@ -1,4 +1,5 @@
 import TicketStatusBadge from "@/components/TicketStatusBadge";
+import TricketPriority from "@/components/TricketPriority";
 import {
   Table,
   TableBody,
@@ -27,7 +28,9 @@ const DataTable = ({ tickets }: Props) => {
               <TableHead>
                 <div className="flex justify-center">Status</div>
               </TableHead>
-              <TableHead>Priority</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
           </TableHeader>
@@ -41,7 +44,11 @@ const DataTable = ({ tickets }: Props) => {
                         <TicketStatusBadge status={ticket.status} />
                       </div>
                     </TableCell>
-                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TricketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
                     <TableCell>
                       {ticket.createdAt.toLocaleDateString("fr-FR", {
                         year: "2-digit",
